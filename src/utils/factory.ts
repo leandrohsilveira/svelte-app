3b88f9216682dc2bcdb386f583c44746fcceb680
leandrohsilveira <leandro.hinckel@gmail.com>
2021-07-28 02:34:24 +0000

    Improve injection engine

--- src/utils/factory.ts
+++ src/utils/factory.ts
@@ -1,24 +1,53 @@
 import { getContext, hasContext, setContext } from 'svelte'
 import type { LoginService } from '../auth'
 
-const SERVICE_FACTORY_CONTEXT_NAME = 'ServiceFactory'
+export interface ContextMap {
+  has(key: string): boolean;
+  get(key: string): any;
+  set(key: string, value: any): void;
+}
 
 export type ServiceFactory = {
   loginService: LoginService
+}
+
+
+const SERVICE_FACTORY_CONTEXT_NAME = 'ServiceFactory'
+
+let contextMap: ContextMap = {
+  has: hasContext,
+  get: getContext,
+  set: setContext,
+}
+
+
+function getServiceFactoryContext(): ServiceFactory {
+  if (contextMap.has(SERVICE_FACTORY_CONTEXT_NAME)) {
+    return contextMap.get(SERVICE_FACTORY_CONTEXT_NAME)
+  }
+  throw new Error('Service factory not provided!')
+}
+
+export function setContextMap(map: ContextMap) {
+  contextMap = map;
 }
 
 export function createServiceFactory(serviceFactory: ServiceFactory) {
   setContext(SERVICE_FACTORY_CONTEXT_NAME, serviceFactory)
 }
 
+export function provideFactory(factory: Partial<ServiceFactory>) {
+  const context: ServiceFactory = contextMap.get(SERVICE_FACTORY_CONTEXT_NAME)
+  contextMap.set(SERVICE_FACTORY_CONTEXT_NAME, {
+    ...(context ?? ({} as any)),
+    ...factory,
+  })
+}
+
 export function getInstance<P extends keyof ServiceFactory>(
   instanceName: P
 ): ServiceFactory[P] {
-  if (hasContext(SERVICE_FACTORY_CONTEXT_NAME)) {
-    const context: ServiceFactory = getContext(SERVICE_FACTORY_CONTEXT_NAME)
-    const instance = context[instanceName]
-    if (instance) return instance
-    else throw new Error(`No instance provided for "${instanceName}"`)
-  }
-  throw new Error('Service factory not provided!')
+  const instance = getServiceFactoryContext()[instanceName]
+  if (instance) return instance
+  else throw new Error(`No instance provided for "${instanceName}"`)
 }
