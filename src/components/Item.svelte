c90dc1a16f2136eaea87dd2a36cafd41a225d163
leandrohsilveira <leandro.hinckel@gmail.com>
2021-07-29 18:18:40 +0000

    Improve Item component

--- src/components/Item.svelte
+++ src/components/Item.svelte
@@ -1,17 +1,19 @@
-
 <script lang="ts">
+  import { createEventDispatcher } from 'svelte'
+
   export let active = false
+
+  const dispatcher = createEventDispatcher<{ click: {} }>()
+  function handleClick() {
+    dispatcher('click', {})
+  }
 </script>
 
-<div class="item" class:active>
+<div class="item" class:active on:click|capture={handleClick}>
   <slot />
 </div>
 
 <style>
-  .item {
-    padding: 5px 20px;
-  }
-
   .item:hover,
   .item.active {
     background-color: #e4e5e6;
@@ -19,5 +21,12 @@
 
   .item:not(:last-child) {
     border-bottom: var(--divider);
+  }
+
+  .item > :global(*) {
+    padding: 5px 20px;
+    display: block;
+    width: 100%;
+    height: max-content;
   }
 </style>
--- src/layout/Layout.svelte
+++ src/layout/Layout.svelte
@@ -1,15 +1,20 @@
 <script lang="ts">
+  import { Link } from 'svelte-routing'
   import { Drawer, Item, Stacked } from '../components'
   import Header from './Header.svelte'
 
   let drawerOpened = false
+
+  function toggleDrawer() {
+    drawerOpened = !drawerOpened
+  }
 </script>
 
 <Header bind:navOpened={drawerOpened} />
 <Drawer bind:open={drawerOpened}>
-  <Item active={true}>Home</Item>
-  <Item>About us</Item>
-  <Item>Contact us</Item>
+  <Item on:click={toggleDrawer}><Link to="/">Home</Link></Item>
+  <Item on:click={toggleDrawer}><Link to="/about">About us</Link></Item>
+  <Item on:click={toggleDrawer}><Link to="/contact">Contact us</Link></Item>
 </Drawer>
 <main>
   <Stacked width="100%" horizontal="center"><slot /></Stacked>
