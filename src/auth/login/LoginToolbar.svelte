79d445851d22dab6d37d5346d3ed8bcf83f959f2
leandrohsilveira <leandro.hinckel@gmail.com>
2021-07-29 12:25:06 +0000

    Add transitions in LoginToolbar

--- src/auth/login/LoginToolbar.svelte
+++ src/auth/login/LoginToolbar.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
+  import { fade } from 'svelte/transition'
+  import Stacked from '../../layout/Stacked.svelte'
   import { getInstance } from '../../utils'
   import type { LoginSubmitEventDetail } from './LoginEvents'
   import LoginForm from './LoginForm.svelte'
 
   export let isLoggingIn = false
 
+  const fadeOptions = { duration: 250 }
   const loginService = getInstance('loginService')
   const authStore = getInstance('authStore')
   const isAuthenticated = authStore.isAuthenticated
@@ -24,13 +27,19 @@
   }
 </script>
 
-{#if isLoggingIn}
-  <span>Logging in...</span>
-{:else if !$isAuthenticated}
-  <LoginForm on:submit={handleSubmit} />
-{:else}
-  <span>Logged in as {$loggedName} ({$loggedUsername})</span>
-  <button class="button button-clear" on:click={handleLogoutClick}
-    >Logout</button
-  >
-{/if}
+<Stacked width={510} height={70} horizontal="right" vertical="middle">
+  {#if isLoggingIn}
+    <div transition:fade={fadeOptions}><span>Logging in...</span></div>
+  {:else if !$isAuthenticated}
+    <div transition:fade={fadeOptions}>
+      <LoginForm on:submit={handleSubmit} />
+    </div>
+  {:else}
+    <div transition:fade={fadeOptions}>
+      <span>Logged in as {$loggedName} ({$loggedUsername})</span>
+      <button class="button button-clear" on:click={handleLogoutClick}
+        >Logout</button
+      >
+    </div>
+  {/if}
+</Stacked>
