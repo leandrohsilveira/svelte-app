7d9ea82b3a178d4aa9d3cf30eeab5a2dd8dd7fed
leandrohsilveira <leandro.hinckel@gmail.com>
2021-07-29 14:35:16 +0000

    Add Layout component

---
+++ src/layout/Layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import Drawer from './Drawer.svelte'
+  import Header from './Header.svelte'
+
+  let drawerOpened = false
+</script>
+
+<Header bind:navOpened={drawerOpened} />
+<Drawer bind:open={drawerOpened} />
+<main />
