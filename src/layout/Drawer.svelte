0819bc054e2c0e0a53f3353bd5bbcc11398dde87
leandrohsilveira <leandro.hinckel@gmail.com>
2021-07-29 14:34:38 +0000

    Add Drawer component

---
+++ src/layout/Drawer.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+  import { fade, fly } from 'svelte/transition'
+  
+  const fadeOptions = { duration: 300 }
+
+  export let open = true
+  export let width = 250
+
+  $: style = `width: ${width}px`
+  $: flyOptions = { duration: 300, x: width * -1, y: 0 }
+
+  function handleOverlayClick() {
+    open = !open
+  }
+</script>
+
+{#if open}
+  <nav class="drawer" {style} transition:fly={flyOptions}>
+    <div class="item active">Home</div>
+    <div class="item">About us</div>
+    <div class="item">Contact us</div>
+  </nav>
+  <div
+    class="overlay"
+    on:click={handleOverlayClick}
+    transition:fade={fadeOptions}
+  />
+{/if}
+
+<style>
+  .overlay {
+    background-color: #00000055;
+    z-index: 19;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+  }
+  .drawer {
+    z-index: 20;
+    display: flex;
+    flex-direction: column;
+    background-color: #f4f5f6;
+    border-right: 0.1rem solid #d1d1d1;
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+  }
+
+  .drawer > .item {
+    padding: 5px 20px;
+  }
+
+  .drawer > .item:hover,
+  .drawer > .item.active {
+    background-color: #e4e5e6;
+  }
+
+  .drawer > .item:not(:last-child) {
+    border-bottom: 0.1rem solid #d1d1d1;
+  }
+</style>
