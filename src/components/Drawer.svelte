c9cf3d4422942282bbbccc5c0f6bd74fd4b06e4a
leandrohsilveira <leandro.hinckel@gmail.com>
2021-07-30 18:21:41 +0000

    Use Slider component in Drawer

--- src/components/Drawer.svelte
+++ src/components/Drawer.svelte
@@ -1,53 +1,29 @@
 <script lang="ts">
-  import { fade, fly } from 'svelte/transition'
-
-  const fadeOptions = { duration: 300 }
+  import Slider from './Slider.svelte'
 
   export let fixed = false
   export let open = true
   export let overlay = true
   export let width = 250
-  export let top = 0
-
-  $: style = `width: ${width}px; top: ${top}`
-  $: flyOptions = { duration: 300, x: width * -1, y: 0 }
-
-  function handleOverlayClick() {
-    open = !open
-  }
 </script>
 
-{#if open || fixed}
-  <nav class="drawer" {style} transition:fly={flyOptions}>
-    <slot />
-  </nav>
-  {#if overlay && !fixed}
-    <div
-      class="overlay"
-      on:click={handleOverlayClick}
-      transition:fade={fadeOptions}
-    />
-  {/if}
+{#if fixed}
+  <Slider open={true} overlay={false} size={width} position="bottom">
+    <nav class="drawer">
+      <slot />
+    </nav>
+  </Slider>
+{:else}
+  <Slider bind:open overlay={overlay && !fixed} size={width} position="bottom">
+    <nav class="drawer">
+      <slot />
+    </nav>
+  </Slider>
 {/if}
 
 <style>
-  .overlay {
-    z-index: var(--overlay-index);
-    background-color: #00000055;
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-  }
   .drawer {
-    z-index: var(--drawer-index);
     display: flex;
     flex-direction: column;
-    background-color: white;
-    border-right: var(--divider);
-    position: fixed;
-    left: 0;
-    bottom: 0;
   }
 </style>
