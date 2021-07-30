e4e13229b1e00ee167fc4b848b8b379c6f449009
leandrohsilveira <leandro.hinckel@gmail.com>
2021-07-30 20:11:18 +0000

    Add user form components

---
+++ src/user/UserForm/UserInfoForm.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import { Field } from "../../components";
+  import { isValid, maxLength, required, validate } from "../../utils";
+
+  export let name: string
+  export let username: string
+  export let valid: boolean
+
+  $: nameErrors = validate(required())(name)
+  $: usernameErrors = validate(required(), maxLength(12))(username)
+  $: valid = isValid(nameErrors, usernameErrors)
+
+</script>
+
+<Field name="name" label="Name:" errors={nameErrors}>
+  <input
+    name="name"
+    autocomplete="name"
+    placeholder="Name"
+    bind:value={name}
+  />
+</Field>
+<Field name="username" label="Username:" errors={usernameErrors}>
+  <input
+    name="username"
+    autocomplete="username"
+    placeholder="Username"
+    bind:value={username}
+  />
+</Field>
---
+++ src/user/UserForm/UserPasswordForm.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+  import { Field } from '../../components'
+
+  import { equalTo, isValid, minLength, required, validate } from '../../utils'
+
+  export let password: string
+  export let valid: boolean
+  export let edit = false
+  let confirmPassword: string
+  let currentPassword: string
+
+  $: passwordErrors = validate(
+    required(),
+    minLength(8),
+    equalTo(confirmPassword, 'The password must be confirmed')
+  )(password)
+  $: confirmPasswordErrors = validate(required(), minLength(8))(confirmPassword)
+  $: currentPasswordErrors = edit
+    ? validate(required(), minLength(8))(currentPassword)
+    : []
+
+  $: valid = isValid(
+    currentPasswordErrors,
+    passwordErrors,
+    confirmPasswordErrors
+  )
+</script>
+
+{#if edit}
+  <Field
+    name="current-password"
+    label="Current password:"
+    errors={currentPasswordErrors}
+  >
+    <input
+      type="password"
+      name="current-password"
+      autocomplete="current-password"
+      placeholder="Current password"
+      bind:value={currentPassword}
+    />
+  </Field>
+{/if}
+<Field name="new-password" label="Password:" errors={passwordErrors}>
+  <input
+    type="password"
+    name="new-password"
+    autocomplete="new-password"
+    placeholder="Password"
+    bind:value={password}
+  />
+</Field>
+<Field
+  name="confirm-password"
+  label="Confirm password:"
+  errors={confirmPasswordErrors}
+>
+  <input
+    type="password"
+    name="confirm-password"
+    autocomplete="new-password"
+    placeholder="Confirm password"
+    bind:value={confirmPassword}
+  />
+</Field>
