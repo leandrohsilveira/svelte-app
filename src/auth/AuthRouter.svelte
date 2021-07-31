3fddc039467e46c9adef1b01a6a2754dd800e684
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2021-07-31 06:26:33 -0300

    Handle signup events

--- src/auth/AuthRouter.svelte
+++ src/auth/AuthRouter.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
   import { Route } from 'svelte-routing'
+  import rootNavigator from '../routes';
   import { AnonymousGuard, AuthenticatedGuard } from './guards'
   import SignupPage from './SignupPage.svelte'
+
 </script>
 
 <Route path="/signup">
-  <AnonymousGuard><SignupPage /></AnonymousGuard>
+  <AnonymousGuard>
+    <SignupPage
+      on:signUpSucessful={rootNavigator.home}
+      on:cancel={rootNavigator.home}
+    />
+  </AnonymousGuard>
 </Route>
 <Route path="/auth/user/edit">
   <AuthenticatedGuard><div>Edit user page :)</div></AuthenticatedGuard>
--- src/auth/SignupPage.svelte
+++ src/auth/SignupPage.svelte
@@ -1,8 +1,30 @@
 <script lang="ts">
+  import { createEventDispatcher } from 'svelte'
+
   import { Page } from '../components'
   import UserForm from '../user/UserForm/UserForm.svelte'
+
+  type Events = {
+    signUpSucessful: void
+    cancel: void
+  }
+
+  const dispatch = createEventDispatcher<Events>()
+
+  function handleSignUp() {
+    dispatch('signUpSucessful')
+  }
+
+  function handleCancel() {
+    dispatch('cancel')
+  }
 </script>
 
 <Page title="Sign-up">
-  <UserForm edit={false} saveButtonLabel="Sign-up" />
+  <UserForm
+    edit={false}
+    saveButtonLabel="Sign-up"
+    on:submit={handleSignUp}
+    on:cancel={handleCancel}
+  />
 </Page>
