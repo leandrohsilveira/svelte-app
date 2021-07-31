b0f0acc7a20dc77423ea1b0f3ed59cd73e5190d5
leandrohsilveira <leandro.hinckel@gmail.com>
2021-07-31 03:52:00 +0000

    Add temporary links to auth routes

--- src/layout/Layout.svelte
+++ src/layout/Layout.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
   import { Link } from 'svelte-routing'
   import { Drawer, Item, Stacked } from '../components'
+  import { getInstance } from '../utils'
   import Header from './Header.svelte'
+
+  const { isAuthenticated } = getInstance('authStore')
 
   let drawerOpened = false
   let innerWidth: number
@@ -18,6 +21,13 @@
 <Drawer bind:open={drawerOpened} {fixed}>
   <h4 class="logo">App</h4>
   <Item on:click={toggleDrawer}><Link to="/">Home</Link></Item>
+  {#if $isAuthenticated}
+    <Item on:click={toggleDrawer}>
+      <Link to="/auth/user/edit">My data</Link>
+    </Item>
+  {:else}
+    <Item on:click={toggleDrawer}><Link to="/signup">Sign-up</Link></Item>
+  {/if}
   <Item on:click={toggleDrawer}><Link to="/about">About us</Link></Item>
   <Item on:click={toggleDrawer}><Link to="/contact">Contact us</Link></Item>
 </Drawer>
