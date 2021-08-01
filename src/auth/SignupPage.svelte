b4fc07ca43f009d2dde8ab608696cd82bbad42a2
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2021-07-31 23:04:51 -0300

    Add signup behaviour to SignupPage

--- src/auth/SignupPage.svelte
+++ src/auth/SignupPage.svelte
@@ -1,18 +1,27 @@
 <script lang="ts">
   import { createEventDispatcher } from 'svelte'
 
+  import type { UserFormData } from '../user'
   import { Page } from '../components'
   import { UserForm } from '../user'
+  import { getInstance } from '../utils'
 
   type Events = {
     signUpSucessful: void
     cancel: void
   }
 
+  let signingUp = false
+
+  const loginService = getInstance('loginService')
   const dispatch = createEventDispatcher<Events>()
 
-  function handleSignUp() {
+  async function handleSignUp(e: CustomEvent<UserFormData>) {
+    const data = e.detail
+    signingUp = true
+    await loginService.signUp(data)
     dispatch('signUpSucessful')
+    signingUp = false
   }
 
   function handleCancel() {
@@ -21,10 +30,14 @@
 </script>
 
 <Page title="Sign-up">
-  <UserForm
-    edit={false}
-    saveButtonLabel="Sign-up"
-    on:submit={handleSignUp}
-    on:cancel={handleCancel}
-  />
+  {#if signingUp}
+    <div>Signing Up...</div>
+  {:else}
+    <UserForm
+      edit={false}
+      saveButtonLabel="Sign-up"
+      on:submit={handleSignUp}
+      on:cancel={handleCancel}
+    />
+  {/if}
 </Page>
